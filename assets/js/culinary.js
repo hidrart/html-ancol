0c6ab2c522dd8c1529ab370f640c1a39cc64793a
Hidrart <hidayattulravli@gmail.com>
2021-10-23 20:01:56 +0700

    revisi

--- assets/js/culinary.js
+++ assets/js/culinary.js
@@ -1,35 +1,51 @@
 var culinary = [{
-    'name': 'Rendang',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
-    'image': '/assets/img/background/rendang.jpg'
+    'name': 'The Pier By Kalaha',
+    'time': '10.00 - 22.00 ',
+    'price': 'Rp.40.000 - Rp.240.000',
+    'description': 'Resto dengan konsep Wharf dengan makanan yang unik dan menarik dengan harga yang bersahabat untuk pengunjung.',
+    'image': '/assets/img/background/pierbykalaha.jpg'
 }, {
-    'name': 'Bakso',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
-    'image': '/assets/img/background/bakso.jpg'
+    'name': 'Colombus Cafe Dermaga',
+    'time': '07.00 - 23.00',
+    'price': 'Rp.20.000 - Rp.230.000',
+    'description': 'Resto dengan konsep outdoor, sangat tepat untuk pergi hangout bareng teman atau kerabat.',
+    'image': '/assets/img/background/colombus.jpg'
 }, {
-    'name': 'Betutu',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
-    'image': '/assets/img/background/betutu.jpg'
+    'name': 'Taman Santap Rumah Kayu',
+    'time': '10.00 - 22.00',
+    'price': 'Rp.10.000 - Rp.250.000',
+    'description': 'Restoran unik yang memberikan sensasi menyantap makanan & minuman di dalam sebuah pesawat Boeing 737.',
+    'image': '/assets/img/background/tamansantap.png'
 }, {
-    'name': 'Bebek Goreng',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
-    'image': '/assets/img/background/bebek-goreng.jpg'
+    'name': 'Le Bridge',
+    'time': '07.00 - 23.00',
+    'price': 'Rp.20.000 - Rp.230.000',
+    'description': 'Restoran yang berlokasi di tengah laut Ancol. terkenal dengan jembatan panjangnya yang akan memberikn nuansa romantis.',
+    'image': '/assets/img/background/lebridge.jpg'
 }, {
-    'name': 'Sate Padang',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
-    'image': '/assets/img/background/sate-padang.jpg'
+    'name': 'Seaside Suki Ancol',
+    'time': '12.00 - 22.00',
+    'price': 'Rp.40.000 - Rp.150.000',
+    'description': 'Restoran thailand dengan buffet service. Menu yang diisajikan yaitu hot pot khas thailand dengan pilihan kuah yang beragam.',
+    'image': '/assets/img/background/seaside.jpg'
 }, {
-    'name': 'Babi panggang',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
-    'image': '/assets/img/background/babi-panggang.jpg'
+    'name': 'Nyiur Cafe Putri duyung ',
+    'time': '10.00  - 23.00',
+    'price': 'Rp.35.000 - Rp.200.000',
+    'description': 'Retoran dengan menu masakan Indonesia, Asia hingga Western. Selain itu restoran ini memilki view pantai yang sangat indah.',
+    'image': '/assets/img/background/nyiur.jpg'
 }, {
-    'name': 'Nasi Goreng',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
-    'image': '/assets/img/background/nasi-goreng.jpg'
+    'name': 'Talaga Sampireun Ancol',
+    'time': '10.00  - 22.00',
+    'price': 'Rp.39.000 - Rp.109.000',
+    'description': 'Restoran khas Sunda yang berkonsep telaga yang dikelilingi oleh Saung. Yang memiliki kesan tradisional pada restoran ini.',
+    'image': '/assets/img/background/talaga.jpg'
 }, {
-    'name': 'Sate',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
-    'image': '/assets/img/background/sate.jpg'
+    'name': 'Segarra Ancol',
+    'time': '12.00  - 24.00',
+    'price': 'Rp.30.000 - Rp.630.000',
+    'description': 'Restaurant dengan konsep western yang akan memberikan kesan khas asli western pada restoran ini.',
+    'image': '/assets/img/background/segarra.jpg'
 }]
 async function ready(fn) {
     if (document.readyState != 'loading') {
@@ -51,12 +67,16 @@ window.ready(async function() {
         food.classList.add('col-12')
         food.classList.add('destination-item')
         food.classList.add('p-.5')
-        food.innerHTML = `<div class=" text-center destination-image" style="background-image: url('${element.image}'); background-size: cover; background-position: center;">
-            <div class="destination-detail">
-                <h3 class="fw-bold my-3">${element.name}</h3>
-                <p>${element.description}</p>
-            </div>
-        </div>`
+        food.innerHTML = `<div class="text-center destination-image" style="background-image: url('${element.image}'); background-size: cover; background-position: center;">
+        <div style="position: absolute; padding: 1em; right: 0">
+            <p style="background-color: #f3c414 ;padding: .3em .8em; border-radius: .5em; color: #ffffff; font-size: .8em; font-weight: 700">${element.time}</p>
+            <p style="background-color: #ff6795;padding: .3em .8em; border-radius: .5em; color: #ffffff;  font-size: .8em; font-weight: 700">${element.price}</p>
+        </div>
+        <div class="destination-detail" style="position: absolute">
+            <h3 class="fw-bold my-3">${element.name}</h3>
+            <p class="my-3">${element.description}</p>
+        </div>
+    </div>`
         culinaryContainer.appendChild(food)
     });
 });
--- assets/js/destination.js
+++ assets/js/destination.js
@@ -1,30 +1,44 @@
 var destinations = [{
     'name': 'Dufan',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+    'time': '10.00 - 17.00',
+    'price': 'Rp 225.000',
+    'description': 'Dufan adalah theme park yang terletak di Taman Impian Jaya Ancol, yang diresmikan dan dibuka untuk umum pada tahun 1985.',
     'image': '/assets/img/background/hero.jpg'
 }, {
     'name': 'Sea World',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+    'time': '10.00 - 16.00',
+    'price': 'Rp 110.000',
+    'description': 'Seaworld Indonesia adalah sebuah miniatur pesona laut yang terdapat dalam kompleks wisata pertama di Taman Impian Jaya Ancol.',
     'image': '/assets/img/background/seaworld.jpg'
 }, {
     'name': 'Ocean Dream Samudra',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+    'time': '09.30 - 16.30',
+    'price': 'Rp 100.000',
+    'description': 'Taman hiburan bertema air yang menampilkan pertunjukan bawah laut, akuarium, & pentas hewan laut.',
     'image': '/assets/img/background/ocean.jpeg'
 }, {
     'name': 'Atlantis Water Adventure',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+    'time': '10.00 - 17.00',
+    'price': 'Rp 125.000',
+    'description': 'Taman hiburan bertema air yang menampilkan pertunjukan bawah laut, akuarium, & pentas hewan laut.',
     'image': '/assets/img/background/atlantis.jpg'
 }, {
     'name': 'Augustine Phinisi',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+    'time': '06.00 - 21.00',
+    'price': 'Rp 674.000',
+    'description': 'Augustine Phinisi bersandar di Taman Impian Jaya Ancol dan menawarkan paket pariwisata Brunch trip dan Sunset trip.',
     'image': '/assets/img/background/augustine.jpeg'
 }, {
     'name': 'Faunaland',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+    'time': '08.00  - 17.00',
+    'price': 'Rp 100.000',
+    'description': 'Kebun binatang kecil dengan singa, keledai & kura-kura raksasa, dan pertunjukan burung & zona interaksi satwa.',
     'image': '/assets/img/background/faunaland.jpg'
 }, {
     'name': 'Gondola Ancol',
-    'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+    'time': '09.00  - 18.00',
+    'price': 'Rp 60.000',
+    'description': 'Gondola Ancol adalah sistem kereta gantung pertama di Indonesia yang menggunakan sistem teknologi yang terkomputerisasi.',
     'image': '/assets/img/background/gondola.jpg'
 }]
 async function ready(fn) {
@@ -47,12 +61,16 @@ window.ready(async function() {
         destination.classList.add('col-12')
         destination.classList.add('destination-item')
         destination.classList.add('p-.5')
-        destination.innerHTML = `<div class=" text-center destination-image" style="background-image: url('${element.image}'); background-size: cover; background-position: center;">
-            <div class="destination-detail">
-                <h3 class="fw-bold my-3">${element.name}</h3>
-                <p>${element.description}</p>
-            </div>
-        </div>`
+        destination.innerHTML = `<div class="text-center destination-image" style="background-image: url('${element.image}'); background-size: cover; background-position: center;">
+        <div style="position: absolute; padding: 1em; right: 0">
+            <p style="background-color: #f3c414 ;padding: .3em .8em; border-radius: .5em; color: #ffffff; font-size: .8em; font-weight: 700">${element.time}</p>
+            <p style="background-color: #ff6795;padding: .3em .8em; border-radius: .5em; color: #ffffff;  font-size: .8em; font-weight: 700">${element.price}</p>
+        </div>
+        <div class="destination-detail" style="position: absolute">
+            <h3 class="fw-bold my-3">${element.name}</h3>
+            <p class="my-3">${element.description}</p>
+        </div>
+    </div>`
         destionationContainer.appendChild(destination)
     });
 });
